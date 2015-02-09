157e80b7264a48a92372ccb20fd775fad64bff9b
jagruti181 <patiljagruti181@gmail.com>
2015-02-09 18:53:28 +0530

    :+1:

--- demo.bat
+++ demo.bat
@@ -1,2 +1,5 @@
 bower install jquery
 bower install angular
+bower install https://github.com/moment/moment.git
+bower install angular-animate
+bower install angularjs-toaster
--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,10 @@
 
 var lat = 0;
 var long = 0;
-var phonecatControllers = angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'restservicemod', 'ngRoute', 'infinite-scroll']);
+var phonecatControllers = angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'restservicemod', 'ngRoute', 'infinite-scroll', 'ngAnimate', 'toaster']);
 
 phonecatControllers.controller('home',
-    function ($scope, TemplateService, NavigationService, RestService, $filter) {
+    function ($scope, TemplateService, NavigationService, RestService, $filter, $location, $timeout, toaster) {
 
         $scope.template = TemplateService;
         $scope.menutitle = NavigationService.makeactive("Home");
@@ -12,6 +12,21 @@ phonecatControllers.controller('home',
         TemplateService.content = "views/home.html";
         $scope.navigation = NavigationService.getnav();
         $scope.coords = {};
+        $scope.showw = false;
+        $scope.pageClass = "page-home";
+    
+        $scope.list = [];
+		for (var i = 0; i < 6; i++) {
+			$timeout(function () {
+				$scope.list.push({ title : "item" });
+			}, 500 * i);
+		};	
+    
+        $scope.myPagingFunction = function (){
+			$timeout(function () {
+				$scope.list.push({ title : "item" });
+			}, 500 * i);
+        }
 
         function showPosition2(position) {
             var latlon = position.coords.latitude + "," + position.coords.longitude;
@@ -20,6 +35,7 @@ phonecatControllers.controller('home',
             $scope.coords = position.coords;
             lat = position.coords.latitude;
             long = position.coords.longitude;
+            $scope.showw = true;
         }
 
         if (navigator.geolocation) {
@@ -40,19 +56,33 @@ phonecatControllers.controller('myprofile',
         $scope.navigation = NavigationService.getnav();
     });
 phonecatControllers.controller('login',
-    function ($scope, TemplateService, NavigationService, RestService, $location) {
+    function ($scope, TemplateService, NavigationService, RestService, $location, toaster) {
         $scope.template = TemplateService;
         TemplateService.title = $scope.menutitle;
         TemplateService.content = "views/login.html";
         $scope.navigation = NavigationService.getnav();
+        $scope.pageClass = "page-login";
+    
+        $scope.loginfunc = function (login) {
+            toaster.pop("error", "Login Error", "Invalid username or password", 5000);
+            toaster.pop("success", "Welcome", "Registered successfully", 5000);
+        }
 
     });
 phonecatControllers.controller('signup',
-    function ($scope, TemplateService, NavigationService, RestService, $location) {
+    function ($scope, TemplateService, NavigationService, RestService, $location, toaster) {
         $scope.template = TemplateService;
         TemplateService.title = $scope.menutitle;
         TemplateService.content = "views/signup.html";
         $scope.navigation = NavigationService.getnav();
+        $scope.pageClass = "page-signup";
+    
+        
+        $scope.signupfunc = function (login){
+            toaster.pop("error", "Signup Error", "Already Exist. Choose Another Email Address", 5000);
+            toaster.pop("success", "Welcome", "Registered successfully", 5000);
+        }
+        
     });
 phonecatControllers.controller('logout',
     function ($scope, TemplateService, NavigationService, RestService, $location) {
